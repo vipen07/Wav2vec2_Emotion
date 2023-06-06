3571eafe0b210e43e2aa9f88981c1ec67361d704
Vipendra Singh <vipenrajput07@gmail.com>
2023-06-06 15:00:20 +0530

    dataprep

--- data_prep.py
+++ data_prep.py
@@ -1,1 +1,109 @@
 
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+from tqdm import tqdm
+
+import torchaudio
+from sklearn.model_selection import train_test_split
+
+import os
+import sys
+
+data = []
+
+for path in tqdm(Path("Data/Acted Emotional Speech Dynamic Database").glob("**/*.wav")):
+
+    name = str(path).split('.')[0].split('/')[-1].split('-')[-1]
+    label = str(path).split('.')[0].split('/')[-2]
+
+    try:
+        # There are some broken files
+        s = torchaudio.load(path)
+        data.append({
+            "name": name,
+            "path": path,
+            "emotion": label
+        })
+    except Exception as e:
+        # print(str(path), e)
+        pass
+
+    # break
+
+
+
+df = pd.DataFrame(data)
+df.tail()
+
+# Filter broken and non-existed paths
+
+print(f"Step 0: {len(df)}")
+
+df["status"] = df["path"].apply(lambda path: True if os.path.exists(path) else None)
+df = df.dropna(subset=["path"])
+df = df.drop("status", 1)
+print(f"Step 1: {len(df)}")
+
+df = df.sample(frac=1)
+df = df.reset_index(drop=True)
+df.head()
+
+print("Labels: ", df["emotion"].unique())
+print()
+df.groupby("emotion").count()[["path"]]
+
+import torchaudio
+import librosa
+import IPython.display as ipd
+import numpy as np
+
+idx = np.random.randint(0, len(df))
+sample = df.iloc[idx]
+path = sample["path"]
+label = sample["emotion"]
+
+
+print(f"ID Location: {idx}")
+print(f"      Label: {label}")
+print()
+
+speech, sr = torchaudio.load(path)
+speech = speech[0].numpy().squeeze()
+speech = librosa.resample(np.asarray(speech), orig_sr = sr, target_sr=16_000)
+ipd.Audio(data=np.asarray(speech), autoplay=True, rate=16000)
+
+save_path = "/content/data"
+
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=101, stratify=df["emotion"])
+
+train_df = train_df.reset_index(drop=True)
+test_df = test_df.reset_index(drop=True)
+
+train_df.to_csv(f"{save_path}/train.csv", sep="\t", encoding="utf-8", index=False)
+test_df.to_csv(f"{save_path}/test.csv", sep="\t", encoding="utf-8", index=False)
+
+
+print(train_df.shape)
+print(test_df.shape)
+
+from datasets import load_dataset, load_metric
+
+
+data_files = {
+    "train": "/content/data/train.csv",
+    "validation": "/content/data/test.csv",
+}
+
+dataset = load_dataset("csv", data_files=data_files, delimiter="\t", )
+train_dataset = dataset["train"]
+eval_dataset = dataset["validation"]
+
+print(train_dataset)
+print(eval_dataset)
+
+input_column = "path"
+output_column = "emotion"
+
+from transformers import AutoConfig, Wav2Vec2Processor
